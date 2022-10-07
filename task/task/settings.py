3e051e88d1bb2d06a932828e9fa8f3267031ba33
Juzer-k <juzerkathiyawadi53@gmail.com>
2022-10-07 12:28:40 +0530

    second commit

--- task/task/settings.py
+++ task/task/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6)*(edy0%xe7d)9socqz-qp9holbr(#0ehh42lw*^j4%mkrweg'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["testdeploy12345.herokuapp.com","localhost"]
 
 # Application definition
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -78,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':  'task',
-        'USER': 'postgres',
-        'PASSWORD': 'juzer5253!',
+        'USER': os.environ['DATA_BASE_USERNAME'],
+        'PASSWORD': os.environ['DATA_BASE_PASSWORD'],
         'PORT': '5432'
         
     }
